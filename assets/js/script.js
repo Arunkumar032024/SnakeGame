876ceb05e18fec7321bbe3d4c9751207950404ff
Arunkumar032024 <arunkumar032024@gmail.com>
2024-06-03 12:14:00 +0530

    Complete Game

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,6 +29,7 @@ $(document).ready(function(){
 
     function isCollide(snake) {
         // If you bump into yourself 
+        
         for (let i = 1; i < snakeArr.length; i++) {
             if(snake[i].x === snake[0].x && snake[i].y === snake[0].y){
                 return true;
@@ -43,7 +44,7 @@ $(document).ready(function(){
 
     function gameEngine(){
         // Part 1: Updating the snake array & Food
-        if(isCollide(snakeArr)){
+        if(isCollide(snakeArr)){            
             gameOverSound.play();
             musicSound.pause();
             inputDir =  {x: 0, y: 0}; 
@@ -51,18 +52,15 @@ $(document).ready(function(){
             snakeArr = [{x: 13, y: 15}];
             musicSound.play();
             score = 0; 
+            $(".score").text(score)
         }
     
         // If you have eaten the food, increment the score and regenerate the food
         if(snakeArr[0].y === food.y && snakeArr[0].x ===food.x){
             foodSound.play();
             score += 1;
-            // if(score>hiscoreval){
-            //     hiscoreval = score;
-            //     localStorage.setItem("hiscore", JSON.stringify(hiscoreval));
-            //     hiscoreBox.innerHTML = "HiScore: " + hiscoreval;
-            // }
-            // scoreBox.innerHTML = "Score: " + score;
+            $(".score").text(score);
+            
             snakeArr.unshift({x: snakeArr[0].x + inputDir.x, y: snakeArr[0].y + inputDir.y});
             let a = 2;
             let b = 16;
